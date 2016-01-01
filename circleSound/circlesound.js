de37fc30d0f25ddaa767e5e1f94a8de2767c6344
jeffreyye <908202572@qq.com>
2016-01-01 15:59:14 +0800

    circleSound

---
+++ circleSound/circlesound.js
@@ -0,0 +1,60 @@
+var numBands = 512;
+function setup() {
+    var soundFile= createAudio('say something.mp3');
+    // say something
+    var thisCanvas = createCanvas(windowWidth, windowHeight); //windowWidth、windowHeight为全局变量
+    thisCanvas.position(0, 0);
+    // colorMode(HSB, 360, 100, 100, 100);
+    // call p5 sound fft function for the number of waves to be analyzed
+    fft = new p5.FFT(0.9, numBands); //此值大小只允许在0-1；
+
+    // call p5 sound library function to get the amplitude of our song
+    amplitude = new p5.Amplitude(0.1); //此处用于控制生成圆点的大小
+    background(0);
+    soundFile.connect(fft);
+    soundFile.connect(amplitude);
+    soundFile.play();
+    // for(var i in amplitude){
+    //     console.log(i+"======="+amplitude[i])
+    // }
+    particles=[];
+    for(var  i =0;i<512;i++){
+        var _particle=new Particles(random(width),random(height));
+        particles.push(_particle);
+    }
+}   
+
+function draw() {
+    freqValues = fft.analyze();
+    waveform=fft.waveform(512);
+
+    volume = amplitude.getLevel();
+    volume_left =amplitude.getLevel(0);
+    fill(0,0,0,volume*255);
+    rect(0,0,width,height);
+    noStroke();
+    fill(0,123,123,volume*255);
+    center();
+
+    for(var i =0;i<particles.length;i++){
+        var p =particles[i];
+        var _speed=1+volume*10+freqValues[i]*i*.1+waveform[i]*.1;
+        p.maxSpeed=map(_speed,0,+_speed,0,8);
+        p.attraction(createVector(width*0.5,height*0.5))
+        p.repulsion(createVector(width*0.5,height*0.5))
+        p.wander(1);
+        p.update();
+        p.display(createVector(width*0.5,height*0.5));
+    }
+    
+}
+
+function center(){
+    radius=volume*1000;
+    ellipse(windowWidth/2, windowHeight/2, radius, radius);
+    radius=volume*1200;
+    ellipse(windowWidth/2, windowHeight/2, radius, radius);
+    radius=volume_left*600;
+    ellipse(windowWidth/2, windowHeight/2, radius, radius);
+}
+
---
+++ circleSound/particles.js
@@ -0,0 +1,65 @@
+var Particles = function(x, y) {
+	this.maxSpeed = 1;
+	this.vel = createVector();
+	this.acc = createVector();
+	this.pos = createVector(x, y);
+}
+Particles.prototype.wander = function(range) {
+	var force = createVector(random(range)*2-range,random(range)*2-range);
+	force.setMag(random(range));//向量长度
+	this.applyForce(force);
+
+}
+// target为Vector类型
+Particles.prototype.attraction = function(target) {
+	var force=p5.Vector.sub(target,this.pos);
+	var d=force.mag();//mag()表示向量长度
+	if(d<500){
+		force.setMag(d*0.01);//正比例函数模型：距离越近的时候，引力越小，距离越大的时候，引力越大。
+		this.applyForce(force);
+	};
+} 
+Particles.prototype.repulsion = function(target) {
+	var force=p5.Vector.sub(this.pos, target)
+	var d=force.mag();
+	if(d<300){
+		force.setMag(amplitude.getLevel()*2000/d);//反比例函数模型：距离越近，斥力越大；距离越远，斥力越小。
+		this.applyForce(force);
+	}
+	if(d>400&&d<600){
+		force.setMag(amplitude.getLevel()*300/d);//反比例函数模型：距离越近，斥力越大；距离越远，斥力越小。
+		this.applyForce(force);
+	}
+
+}
+Particles.prototype.colorChange = function(target) {
+	var force=p5.Vector.sub(this.pos, target)
+	var d=force.mag();
+	if(d>200){
+		var c=color(random(255),random*(255),random(255));
+		fill(c,100);
+	}
+}
+Particles.prototype.applyForce = function(force) {
+	this.acc.add(force);
+}
+Particles.prototype.update = function() {
+	this.vel.add(this.acc);
+	this.vel.limit(this.maxSpeed);//限制它的最大速度
+	this.pos.add(this.vel);
+	this.acc.mult(0);
+}
+Particles.prototype.display = function(target) {
+	var force=p5.Vector.sub(this.pos, target)
+	var d=force.mag();
+	if(d<60){
+		stroke(0,volume*255,map(volume*255,0,255,100,255),map(d,0,80,255,0));
+	}else if(d<100&&d>50){
+		stroke(255,volume*255,100,map(d,80,130,50,0));
+	}else{	
+		stroke(255);
+	}
+	var len=this.vel.mag();
+	strokeWeight(map(len,0,len,1,3));
+	point(this.pos.x, this.pos.y)
+}
