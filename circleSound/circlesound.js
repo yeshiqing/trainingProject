7008bdbd2a2a6cd16a4eea02f02ca449daf0ee54
jeffreyye <908202572@qq.com>
2016-01-01 20:43:26 +0800

    alert FFT

--- circleSound/circlesound.js
+++ circleSound/circlesound.js
@@ -1,26 +1,20 @@
 var numBands = 512;
-
+function preload(){
+    message_anima();
+   
+}
 function setup() {
-    alert(0)
     var soundFile = createAudio('say something.mp3');
-    // say something
-    var thisCanvas = createCanvas(windowWidth, windowHeight); //windowWidth、windowHeight为全局变量
-    alert(1)
-    thisCanvas.position(0, 0);
-    // colorMode(HSB, 360, 100, 100, 100);
-    // call p5 sound fft function for the number of waves to be analyzed
-    alert(new p5.FFT(0.9,numBands))
+    alert(p5)
+    alert(p5.FFT)
     fft = new p5.FFT(0.9, numBands); //此值大小只允许在0-1；
-alert("fft")
-    // call p5 sound library function to get the amplitude of our song
     amplitude = new p5.Amplitude(0.1); //此处用于控制生成圆点的大小
-    background(0);
     soundFile.connect(fft);
     soundFile.connect(amplitude);
+    var thisCanvas = createCanvas(windowWidth, windowHeight); //windowWidth、windowHeight为全局变量
+    thisCanvas.position(0, 0);
+    background(0);
     soundFile.play();
-    // for(var i in amplitude){
-    //     console.log(i+"======="+amplitude[i])
-    // }
     particles = [];
     for (var i = 0; i < 512; i++) {
         var _particle = new Particles(random(width), random(height));
@@ -50,7 +44,6 @@ function draw() {
         p.update();
         p.display(createVector(width * 0.5, height * 0.5));
     }
-
 }
 
 function center() {
