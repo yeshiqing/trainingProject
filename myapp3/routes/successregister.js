87e80a8173f65156e63ca3f31f97456e8a2b5880
jeffreyye <908202572@qq.com>
2017-04-02 06:25:09 +0800

    remove myapp3

--- myapp3/routes/successregister.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/',function(req,res,next){
-	res.render('successregister',{title:'registersuccess'});
-});
-
-module.exports = router;
--- myapp3/server.js
+++
@@ -1,8 +0,0 @@
-var app = require('./app.js');
-
-
-var server = app.listen('3000','192.168.0.99',function(e){
-	var host = server.address().address;
-	var port = server.address().port;
-	console.log('example app listening at http://%s:%s',host,port);
-});
